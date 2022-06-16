64364f20da2f228475b5749ab75395d12ec8ddf2
Jack Firth <jackhfirth@gmail.com>
2022-06-15 19:24:45 -0700

    Fill out template

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
-(define collection "racket-package-template")
+(define collection "syntax-render")
 
 (define scribblings
   (list (list "main.scrbl"
               (list 'multi-page)
               (list 'library)
-              "racket-package-template")))
+              "syntax-render")))
 
 (define deps
   (list "base"))
--- main.scrbl
+++ main.scrbl
@@ -1,4 +1,4 @@
 #lang scribble/manual
 
-@title{Racket Package Template}
-@defmodule[racket-package-template]
+@title{Syntax Render}
+@defmodule[syntax-render]
